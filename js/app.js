6b31213b5dff043368d8b08440bbd88b8083cfb0
Courtney Topping <Courtney@Courtneys-MacBook-Pro.local>
2019-09-20 11:30:30 -0400

    portfolio page elements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation()
+
+window.changeImage = function() {
+    element=document.getElementById('myimage');
+    if (element.src.match("bulbon")) {
+        element.src="img/info1.png";
+    }
+    else { element.src="img/info2.png";
+    }
+}
+
+window.changeImage2 = function() {
+    element=document.getElementById('myimage2');
+    if (element.src.match("bulbon2")) {
+        element.src="img/info3.png";
+    }
+    else { element.src="img/info4.png";
+    }
+}
